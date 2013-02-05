7c45d0a02b8b046df889c4a6d22b8f74c013836b
Cesar Andreu <cesarandreu@gmail.com>
2013-02-05 02:55:36 -0400

    Added favicon. Added source support. Added about page.

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ mongoose.connect('mongodb://localhost/kwoter');
 var KwoterSchema = new mongoose.Schema({
   quote: String,
   author: String,
-  url: String
+  url: String,
+  source: String
 });
 
 var Kwoter = mongoose.model('Kwoter', KwoterSchema, 'kwoter', false);
@@ -25,7 +26,7 @@ app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
-  app.use(express.favicon());
+  app.use(express.favicon(__dirname + '/public/images/favicon.ico'));
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
@@ -41,26 +42,6 @@ app.get('/', function(req, res) {
   res.render('index', {error: false, length: false});
 });
 
-app.get('/q/:id', function(req, res) {
-  if(!req.params.id){
-      res.render('quote', {exist: false, url: '', quote: '', author: '', host: req.host});
-  } else{
-    Kwoter.findOne({url: req.params.id}).exec(function(err, result) {
-      if(err){
-        console.log('Error: ' + err);
-        res.render('quote', {exist: false, url: '', quote: '', author: '', host: req.host});
-      } else{
-        if(!result){
-          res.render('quote', {exist: false, url: '', quote: '', author: '', host: req.host});
-        } else{
-          res.render('quote', {exist: true, url: result.url, quote: result.quote, author: result.author, host: req.host});
-        }
-      }
-    });
-  }
-
-});
-
 //Someone submits a new quote.
 app.post('/', function(req, res) {
 
@@ -69,7 +50,7 @@ app.post('/', function(req, res) {
     res.render('index', {error: true, length: false});
 
   //Checks if it does not pass the character limit. 
-  } else if(req.body.quote.length >= 500 || req.body.author.length >= 100){
+  } else if(req.body.quote.length >= 500 || req.body.author.length >= 50){
     res.render('index', {error: false, length: true});
 
   } else{
@@ -89,9 +70,19 @@ app.post('/', function(req, res) {
         //Otherwise it stores the quote in the database.
         } else{
 
+          //Quote source
+          var sourceText;
+          if(req.body.source){
+            sourceText = req.body.source;
+          } else {
+            sourceText = '';
+          }
+
           //Quote URL
+          //Need to add a check here to make sure that the quoteURL is not repeteated.
           var quoteURL = shortId.generate();
-          var newQuote = new Kwoter({quote: req.body.quote, author: req.body.author, url: quoteURL});
+
+          var newQuote = new Kwoter({quote: req.body.quote, author: req.body.author, url: quoteURL, source: sourceText});
           newQuote.save(function(err) {
             if(err){
               console.log('Error: ' + err);
@@ -107,6 +98,29 @@ app.post('/', function(req, res) {
 });
 
 
+app.get('/q/:id', function(req, res) {
+  if(!req.params.id){
+      res.render('quote', {exist: false, url: '', quote: '', author: '', host: req.host, source: ''});
+  } else{
+    Kwoter.findOne({url: req.params.id}).exec(function(err, result) {
+      if(err){
+        console.log('Error: ' + err);
+        res.render('quote', {exist: false, url: '', quote: '', author: '', host: req.host, source: ''});
+      } else{
+        if(!result){
+          res.render('quote', {exist: false, url: '', quote: '', author: '', host: req.host, source: ''});
+        } else{
+          res.render('quote', {exist: true, url: result.url, quote: result.quote, author: result.author, host: req.host, source: result.source});
+        }
+      }
+    });
+  }
+
+});
+
+app.get('/about', function(req, res) {
+  res.render('about');
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
